3faf5f70647cc7357bd9487a2b317ca2f4bf4e0e
Sandeep <sandeepgh2002@gmail.com>
2023-04-22 17:33:41 +0530

    first commit

---
+++ todoList/urls.py
@@ -0,0 +1,11 @@
+
+from django.contrib import admin
+from django.urls import path, include
+from api.views import apiOverview
+from django.views.generic import TemplateView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),
+    path('', TemplateView.as_view(template_name='index.html')),
+]
